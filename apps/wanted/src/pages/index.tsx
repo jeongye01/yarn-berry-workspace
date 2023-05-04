8043537932d139d086753ff775e4c704933597fc
jeongye01@wavedeck.ai <74299317+jeongye01@users.noreply.github.com>
2023-05-04 11:48:12 +0900

    init

---
+++ apps/wanted/src/pages/index.tsx
@@ -0,0 +1,15 @@
+import Image from "next/image";
+import { sayHello } from "@wanted/lib";
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export default function Home() {
+  return (
+    <main
+      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
+    >
+      <p>의존성 테스트 {sayHello()}</p>
+    </main>
+  );
+}
